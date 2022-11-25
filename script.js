4cd2f00b35cead795e92cb78f63f36ad7ccd78c6
Christopher Shaw <silvertn350@yahoo.com>
2022-11-25 13:06:44 -0600

    1st commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('.container')
+const signUpBtn = document.querySelector('.green-bg button')
+
+
+signUpBtn.addEventListener('click',()=>{
+    container.classList.toggle('change')
+})
